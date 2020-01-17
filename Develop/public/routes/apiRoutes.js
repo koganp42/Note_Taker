8367998fb77137ee806088a3bfd8022fd30833df
Kogan <koganp42@gmail.com>
2020-01-17 12:17:08 -0600

    Installed node packets, wrote all html routes and get&post api routes

---
+++ Develop/public/routes/apiRoutes.js
@@ -0,0 +1,51 @@
+const fs = require("fs");
+async function dbDataRead(){ 
+    fs.readFile('../db/db.json', function read(err, data) {
+    if (err) {
+        throw err;
+    }
+    return(data);
+  })};
+
+module.exports = function(app) {
+  
+    app.get("/api/notes", function(req, res) {
+      let data = await dbDataRead(); 
+      res.json(JSON.parse(data));
+    });
+  
+    // API POST Requests
+    // Below code handles when a user submits a form and thus submits data to the server.
+    // In each of the below cases, when a user submits form data (a JSON object)
+    // ...the JSON is pushed to the appropriate JavaScript array
+    // (ex. User fills out a reservation request... this data is then sent to the server...
+    // Then the server saves the data to the tableData array)
+    // ---------------------------------------------------------------------------
+  
+    app.post("/api/notes", function(req, res) {
+        let data = await dbDataRead();
+        let parsedData = JSON.parse(data);
+        parsedData.push(req.body);
+        let jsonData = JSON.stringify(parsedData);
+        fs.writeFileSync("../db/db.json", jsonData, function(Error){
+            if(Error){
+                throw(Error)
+            } else {
+                res.json(req.body)
+            };
+        });
+    });
+  
+    // ---------------------------------------------------------------------------
+    // I added this below code so you could clear out the table while working with the functionality.
+    // Don"t worry about it!
+  
+    app.delete("/api/clear", function(req, res) {
+      // Empty out the arrays of data
+      tableData.length = 0;
+      waitListData.length = 0;
+  
+      res.json({ ok: true });
+    });
+  };
+  
---
+++ Develop/public/routes/htmlRoutes.js
@@ -0,0 +1,17 @@
+const path = require("path");
+
+module.exports = function(app) {
+    // HTML GET Requests
+    app.get("/index", function(req, res) {
+      res.sendFile(path.join(__dirname, "../public/index.html"));
+    });
+  
+    app.get("/notes", function(req, res) {
+      res.sendFile(path.join(__dirname, "../public/notes.html"));
+    });
+  
+    // If no matching route is found default to index
+    app.get("*", function(req, res) {
+      res.sendFile(path.join(__dirname, "../public/index.html"));
+    });
+  };
