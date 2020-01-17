bb5e94559579c385e342ff9ea2ecd53f8d0b2acc
Kogan <koganp42@gmail.com>
2020-01-17 12:52:08 -0600

    Added routing for the index js, corrected syntax of other html routes

--- Develop/public/routes/htmlRoutes.js
+++ Develop/public/routes/htmlRoutes.js
@@ -3,19 +3,23 @@ const path = require("path");
 module.exports = function(app) {
     // HTML GET Requests
     app.get("/index", function(req, res) {
-      res.sendFile(path.join(__dirname, "../public/index.html"));
+      res.sendFile(path.join(__dirname, "../index.html"));
     });
   
     app.get("/notes", function(req, res) {
-      res.sendFile(path.join(__dirname, "../public/notes.html"));
+      res.sendFile(path.join(__dirname, "../notes.html"));
     });
 
-    app.get("/notes", function(req, res) {
-        res.sendFile(path.join(__dirname, "../public/assets/css/styles.css"));
+    app.get("/assets/js/index.js", function(req, res) {
+        res.sendFile(path.join(__dirname, "../assets/js/index.js"));
+      });
+
+    app.get("/assets/css/styles.css", function(req, res) {
+        res.sendFile(path.join(__dirname, "../assets/css/styles.css"));
       });
 
     // If no matching route is found default to index
     app.get("*", function(req, res) {
-      res.sendFile(path.join(__dirname, "../public/index.html"));
+      res.sendFile(path.join(__dirname, "../index.html"));
     });
   };
